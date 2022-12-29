4b23a40bf5010f266f3f62ace9b07bbe8772330b
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-12-29 11:48:23 -0300

    OrderStatus enum

--- course/src/main/java/com/springapi/course/entities/Order.java
+++ course/src/main/java/com/springapi/course/entities/Order.java
@@ -1,6 +1,7 @@
 package com.springapi.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.springapi.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -20,6 +21,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "clientId")
     private User client;
@@ -28,10 +31,22 @@ public class Order implements Serializable {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+
+        }
+        this.orderStatus = orderStatus.getCode();
     }
 
     public Long getId() {
